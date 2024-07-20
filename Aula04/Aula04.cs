d3774dcf53933065f2570b43bea578b09d34faff
feliperyo <ryofeliperyo@gmail.com>
2024-07-20 11:36:03 -0300

    escopo de variáveis

---
+++ Aula04/Aula04.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Aula04
+{
+    int num = 10;
+    static void Main()
+    {
+        int num2 = 0;
+    }
+
+    void teste()
+    {
+        int num3 = 0;
+        Console.WriteLine(num);
+    }
+}
