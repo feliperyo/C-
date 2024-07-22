a89b4a68fdf49e63bae4c76ed8b4100a50ad5608
feliperyo <ryofeliperyo@gmail.com>
2024-07-22 12:08:46 -0300

    debug

--- Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
+++ Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c200a29ac8e7d5813eaab6cb89570f3f152acb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3de7dd77d474cfa5a5932478dd11de85768be413")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
