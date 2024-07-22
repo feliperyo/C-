438fd31e218618b0bb4eadca3a79e81e334d9281
feliperyo <ryofeliperyo@gmail.com>
2024-07-22 13:47:05 -0300

    Conversões de tipos (typecast)

---
+++ Aula11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Aula11
+{
+    static void Main()
+    {
+        int n1 = 10;
+        float n2 = n1;
+
+        float n3 = 10.5f;
+        int n4 = (int)n3; //type cast
+
+        int vInt = 10;
+        short vLong = (short)vInt;
+
+        Console.WriteLine(n2);
+        Console.WriteLine(n4);
+        Console.WriteLine(vLong);
+    }
+}
