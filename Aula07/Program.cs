1d3cbb9c71099cb7821f55babce6f24d7f9190e2
feliperyo <ryofeliperyo@gmail.com>
2024-07-22 11:21:29 -0300

    const em C#

---
+++ Aula07/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Aula07
+{
+    static void Main()
+    {
+        const string nome = "  Felipe";
+        const double pi = 3.1415;
+
+        Console.WriteLine("Nome: {0}\nPi: {1}", nome, pi);
+    }
+}
