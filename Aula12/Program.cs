91c9bf1db471246ae05405c6a1317c770c1647ce
feliperyo <ryofeliperyo@gmail.com>
2024-07-22 14:10:25 -0300

    Condicional IF

---
+++ Aula12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Aula12
+{
+    static void Main()
+    {
+        int n1, n2, n3, n4, res;
+        res = n1 = n2 = n3 = n4 = 0;
+
+        Console.Write("Digite a nota 1: ");
+        n1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a nota 2: ");
+        n2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a nota 3: ");
+        n3 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a nota 4: ");
+        n4 = int.Parse(Console.ReadLine());
+
+        res = (n1 + n2 + n3 + n4) / 4;
+
+        if (res >= 6)
+        {
+            Console.WriteLine("Nota: {0} - Aprovado", res);
+        }
+        else
+        {
+            Console.WriteLine("Nota: {0} - Reprovado", res);
+        }
+    }
+}
