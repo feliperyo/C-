37454b15abf0108e2c4fb613332987db89a29eef
feliperyo <ryofeliperyo@gmail.com>
2024-07-23 16:25:56 -0300

    else if

---
+++ Aula13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Aula13
+{
+    static void Main()
+    {
+        int n1, n2, n3, n4, res;
+        res = n1 = n2 = n3 = n4 = 0;
+        string resultado;
+
+        Console.Write("Digite a nota 1: ");
+        n1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a nota 2: ");
+        n2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a nota 3: ");
+        n3 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a nota 4: ");
+        n4 = int.Parse(Console.ReadLine());
+
+        res = (n1 + n2 + n3 + n4) / 4;
+
+        if (res >= 6)
+        {
+            resultado = "Aprovado";
+        }
+        else if (res == 5)
+        {
+            resultado = "Recuperação";
+        }
+        else
+        {
+            resultado = "Reprovado";
+        }
+
+        Console.WriteLine("Nota: {0} - Resultado: {1}", res, resultado);
+
+    }
+}
